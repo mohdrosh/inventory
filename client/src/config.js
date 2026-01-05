9fec713c700a06a593662b217f76e7cd7e2edf7d
Hakoder <timemind98@gmail.com>
2026-01-05 13:19:37 +0530

    backend url updated

--- client/src/config.js
+++ client/src/config.js
@@ -4,7 +4,7 @@ const isDevelopment = window.location.hostname === 'localhost' ||
 
 export const API_BASE_URL = isDevelopment 
   ? 'http://localhost:5000/api'
-  : "https://spring8-inventory-management.onrender.com";
+  : "https://spring8-inventory-management.onrender.com/api";
 
 export const config = {
   apiUrl: API_BASE_URL,
